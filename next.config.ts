1b9ed6fe7bbe813ea94f759f1329c5b31b29fa2c
Augusto Carvalho <augustofilho@prodam.sp.gov.br>
2025-07-22 14:13:56 -0300

    feat: adicionados componentes Header e Footer, Conteúdo da página inicial e atualização do favicon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
+    if (!isServer) {
+      if (config.resolve) {
+        config.resolve.fallback = {
+          ...(config.resolve.fallback || {}),
+          fs: false,
+        };
+      } else {
+        config.resolve = {
+          fallback: { fs: false },
+        };
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Top.tsx
@@ -0,0 +1,22 @@
+export default function Top() {
+  return (
+    <header className="w-screen bg-green-300 h-20 text-zinc-50 font-bold flex items-center justify-between px-8">
+
+      <div className="text-lg flex gap-4 items-center">
+        <img src='./head-logo2.svg' className="w-12"/>
+        COGNITIVA
+      </div>
+
+      <nav className="flex gap-20 text-md">
+        <a href="#" className="hover:underline">Sobre nós</a>
+        <a href="#" className="hover:underline">Notícias</a>
+        <a href="#" className="hover:underline">Contato</a>
+      </nav>
+
+      <div className="flex items-center gap-4">
+        <button className="bg-zinc-50 text-green-300 px-4 py-1 rounded hover:bg-green-100 transition">Login</button>
+        <div className="w-8 h-8 bg-gray-400 rounded-full"></div> {/* Imagem placeholder */}
+      </div>
+    </header>
+  );
+}
